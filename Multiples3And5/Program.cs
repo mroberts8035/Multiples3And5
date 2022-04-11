a99a7711c6d170fe4bad2dc275e4cc53c9aa3ccb
Matthew Roberts <mroberts8035@outlook.com>
2022-04-11 15:58:10 -0500

    Completed coding challenge

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -10,6 +10,27 @@ namespace Multiples3And5
     {
         static void Main(string[] args)
         {
+            /*
+             If we list all the natural numbers below 10
+                that are multiples of 3 or 5, we get 3, 5,
+                6 and 9. The sum of these multiples is 23.
+
+                Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
+             */
+
+            var multiples = new List<int>();
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    multiples.Add(i);
+                    //Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine($"The sum of all the multiples of either 3 or 5 below 1000 equals: {multiples.Sum()}");
+
         }
     }
 }
