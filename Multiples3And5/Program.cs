e3816399e253dc2f695893a121f4e88c6cd02773
Matthew Roberts <mroberts8035@outlook.com>
2022-04-11 16:03:33 -0500

    Added comments and updated formatting

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -18,18 +18,24 @@ namespace Multiples3And5
                 Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
              */
 
+            //Initated a list to add the multiples to
             var multiples = new List<int>();
 
+            //Created a For loop to check the numbers below 1000
             for (int i = 1; i < 1000; i++)
             {
+                //Used modulus to see if the numbers are a multiple of either 3 or 5
                 if (i % 3 == 0 || i % 5 == 0)
                 {
+                    //If the numbers are a multiple add it to the list created above
                     multiples.Add(i);
+                    //This CW is to test that I am getting the right stuff added to the list then I commented it out
                     //Console.WriteLine(i);
                 }
             }
 
-            Console.WriteLine($"The sum of all the multiples of either 3 or 5 below 1000 equals: {multiples.Sum()}");
+            //Printing out a sum of the list with a string for better UX
+            Console.WriteLine($"The sum of all the multiples of either 3 or 5 below 1000 equals: {multiples.Sum():n0}");
 
         }
     }
